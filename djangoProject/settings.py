e347602039cb9f6186e3663ce4da3e3643663c4f
Kareem <mikun4kareem@gmail.com>
2024-07-27 13:24:53 +0100

    allowed host update

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = 'django-insecure-9_bul-2v3)z@(%*n9#j%1z0xm=9rxn@yq!t^%uk%@k=z_=$mmg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["10.0.2.2", "127.0.0.1", "10.0.10.233"]
+ALLOWED_HOSTS = ["10.0.2.2", "127.0.0.1", "taufoods.onrender.com"]
 
 # Application definition
 
